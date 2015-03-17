e037ff3c846b4732cc032274cbaaa38ce0f33c96
David Blackmon <davidkblackmon@gmail.com>
2015-03-17 03:23:46 -0400

    general refactor and correct spelling

--- src/main/scala/com/davidkblackmon/enigma.scala
+++ src/main/scala/com/davidkblackmon/enigma.scala
@@ -11,33 +11,30 @@ object Enigma {
   val charList = ('A' to 'Z').toList
   val plugCount = 10
 
-  def buildReflector(size: Int, seed: Int): Reflector = {
-    val chars = shuffledChars(seed).take(size)
+  def buildReflector(size: Int): Reflector = {
+    val chars = shuffledChars.take(size)
     Reflector(chars.take(size/2).zip(chars.drop(size/2)).flatMap(x => List(x,x.swap)).toMap)
   }
 
-  def shuffledChars(seed: Int): List[Char] = {
-    (new scala.util.Random(seed)).shuffle(charList)
+  def buildNullReflector(size: Int): Reflector = {
+    val chars = shuffledChars.take(size)
+    Reflector(charList.zip(charList).toMap)
   }
 
-  def buildPlugBoard(seed: Int): Reflector = {
-    buildReflector(plugCount,seed)
-  }
+  def shuffledChars(): List[Char] = scala.util.Random.shuffle(charList)
 
-  def buildRotor(seed1: Int, seed2: Int): Rotor = {
-    Rotor(shuffledChars(seed1), shuffledChars(seed2))
-  }
+  def buildPlugBoard(): Reflector = buildReflector(plugCount)
 
-  def readRotor(r: Rotor)(reflected: Boolean)(position: Int)(index: Int)(input: Char): Char = {
-    if (reflected)
-      read(r.sideB,rotate(r.sideA, rotationDist(position, index)))(input)
-    else
-      read(rotate(r.sideA, rotationDist(position, index)),r.sideB)(input)
-  }
+  def buildRotor(): Rotor = Rotor(shuffledChars, shuffledChars)
 
-  def read(inputSide: List[Char], outputSide: List[Char])(input: Char): Char = {
+  def readReflectedRotor(r: Rotor)(position: Int)(index: Int)(input: Char): Char =
+    read(r.sideB, rotate(r.sideA, rotationDist(position, index)))(input)
+
+  def readRotor(r: Rotor)(position: Int)(index: Int)(input: Char): Char =
+    read(rotate(r.sideA, rotationDist(position, index)),r.sideB)(input)
+
+  def read(inputSide: List[Char], outputSide: List[Char])(input: Char): Char =
     outputSide(inputSide.indexOf(input))
-  }
 
   def rotationDist(position: Int, index: Int): Int = position match {
     case 0 => index % 26
@@ -46,20 +43,19 @@ object Enigma {
     case _ => 0
   }
 
-  def rotate[A](xs: List[A], dist: Int): List[A] = {
+  def rotate[A](xs: List[A], dist: Int): List[A] =
     xs.drop(dist) ::: xs.take(dist)
-  }
 
-  def engimaize(machine: Machine, input: String): String = {
+  def enigmaize(machine: Machine, input: String): String = {
     input.zipWithIndex.map{ case (c,i) =>
       val push = machine.plugBoard.read _ andThen
-        readRotor(machine.r1)(false)(0)(i)_ andThen
-        readRotor(machine.r2)(false)(1)(i)_ andThen
-        readRotor(machine.r3)(false)(2)(i)_ andThen
+        readRotor(machine.r1)(0)(i)_ andThen
+        readRotor(machine.r2)(1)(i)_ andThen
+        readRotor(machine.r3)(2)(i)_ andThen
         machine.reflector.read _ andThen
-        readRotor(machine.r3)(true)(2)(i)_ andThen
-        readRotor(machine.r2)(true)(1)(i)_ andThen
-        readRotor(machine.r1)(true)(0)(i)_ andThen
+        readReflectedRotor(machine.r3)(2)(i)_ andThen
+        readReflectedRotor(machine.r2)(1)(i)_ andThen
+        readReflectedRotor(machine.r1)(0)(i)_ andThen
         machine.plugBoard.read _
 
       push(c)
